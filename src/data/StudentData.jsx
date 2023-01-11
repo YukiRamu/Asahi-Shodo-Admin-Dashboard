e78f125564d2d2d227df874c9f4ee5b799b7f5e9
YukiRamu <yukipooh4120@gmail.com>
2023-01-10 19:33:20 -0800

    API connected

---
+++ src/data/StudentData.jsx
@@ -0,0 +1,16 @@
+﻿//data fetch
+
+export const getData = async () => {
+  try {
+    //API data fetch
+    const res = await fetch('https://randomuser.me/api/?results=50');
+    if (!res.ok) {
+      throw res.statusText;
+    } else {
+      const studentData = await res.json();
+      return studentData.results;
+    }
+  } catch (error) {
+    console.error(`Failed to fetch student Data. Error= ${error}`);
+  }
+};
--- src/features/studentList/StudentList.jsx
+++ src/features/studentList/StudentList.jsx
@@ -7,12 +7,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import { sortAsc, sortDesc, getAllStudent } from '../../action/Action';
 import rootReducer from '../../reduer';
 import StudentModal from '../studentModal/StudentModal';
+import { getData } from '../../data/StudentData';
+import uuid from 'react-uuid';
 
 const StudentList = () => {
 
   //redux
   const studentData = useSelector(state => state.rootReducer.student);
   const dispatch = useDispatch();
+
+  console.log('data is ready', studentData);
 
   //private state
   const [sortOrder, setSortOrder] = useState('asc');
@@ -21,8 +25,15 @@ const StudentList = () => {
 
   //When the component is first mounted
   useEffect(() => {
-    dispatch(getAllStudent(data.student)); //will be connected to mongo DB
+    (async () => {
+      const data = await getData();
+      console.log(data);
+      if (data.length !== 0) {
+        dispatch(getAllStudent(data));
+      }
+    })();
   }, []);
+
 
   //method
   const sortStudentList = (item, order) => {
@@ -54,8 +65,8 @@ const StudentList = () => {
                   <th>Name {sortOrder === 'asc' ?
                     <FaSortAlphaDown onClick={() => sortStudentList('name', 'desc')} /> :
                     <FaSortAlphaUpAlt onClick={() => sortStudentList('name', 'asc')} />}</th>
-                  <th>Kana</th>
-                  <th>Class {sortOrder === 'asc' ?
+                  <th>Gender</th>
+                  <th>Location {sortOrder === 'asc' ?
                     <FaSortDown onClick={() => sortStudentList('class', 'desc')} /> :
                     <FaSortUp onClick={() => sortStudentList('class', 'asc')} />}</th>
                 </tr>
@@ -64,10 +75,10 @@ const StudentList = () => {
                 {
                   studentData.studentList.map((elem, index) => (
                     <tr key={index} onClick={() => openDetail(elem.id)}>
-                      <td className='studentId'>{elem.id}</td>
-                      <td className='studentData'>{elem.name}</td>
-                      <td className='studentData'>{elem.kana}</td>
-                      <td className='studentData'>{elem.class}</td>
+                      <td className='studentId'>{index+1}</td>
+                      <td className='studentData'>{elem.name.first} {elem.name.last} ({elem.dob.age})</td>
+                      <td className='studentData'>{elem.gender}</td>
+                      <td className='studentData'>{elem.location.country}</td>
                     </tr>
                   ))
                 }
